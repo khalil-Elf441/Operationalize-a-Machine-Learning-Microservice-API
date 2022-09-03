cd3392e985086b3b11ae3c0c4c9910c97044229a
Khalil EL <elfallahikhalil@gmail.com>
2022-09-03 04:33:15 +0000

    Kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=khalilelf441/ml-microservice-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-microservice-api \
+    --image=$dockerpath \
+    --port=80 \
+    --labels app=ml-microservice-api
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward ml-microservice-api 8000:80
 
