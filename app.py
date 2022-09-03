1858cdf73d28a66c6053cd6f306fe3ce40d3cd89
Khalil EL <elfallahikhalil@gmail.com>
2022-09-03 03:14:44 +0000

    upload_docker.sh

--- app.py
+++ app.py
@@ -62,7 +62,8 @@ def predict():
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
-    # TO DO:  Log the output prediction value
+    # Finished TO DO:  Log the output prediction value
+    LOG.info(f"Output prediction: {prediction}")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=khalilelf441/ml-microservice-api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=khalilelf441
+docker tag ml-microservice-api $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
